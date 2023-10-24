42f622ee1e38f96f14a888573ddbb3f729322973
Tiffany Brown <tdbrown_2001@yahoo.com>
2023-10-24 17:37:15 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/bookshelf.js
@@ -0,0 +1,23 @@
+const bookshelf = [
+  {
+    isbn: '978-1491929483',
+    title:
+      'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+    author: 'Lindsay Bassett',
+  },
+  {
+    isbn: '978-1484218624',
+    title: 'JSON Quick Syntax Reference',
+    author: 'Wallace Jackson',
+  },
+  {
+    isbn: '978-0692232699',
+    title:
+      "Build APIs You Won't Hate: Everyone and their dog wants an API, so you should probably learn how to build them",
+    author: 'Phil Sturgeon & Laura Bohill',
+  },
+];
+
+console.log('Value of bookshelf 2nd author:', bookshelf[1].author);
+console.log('Value of bookshelf 1st ISBN:', bookshelf[0].isbn);
+console.log('Value of bookshelf 3rd title:', bookshelf[2].title);
---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,49 @@
+const orderHistory = [
+  {
+    title: 'JavaScript for Impatient Programmers',
+    author: 'Dr. Axel Rauschmayer',
+    total: 34,
+    'order #': 11439416898772232,
+    'order placed': 'August 4, 2020',
+    delivered: 'August 8, 2020',
+    'return window closed': 'September 7, 2020',
+  },
+  {
+    title: 'The Timeless Way of Building',
+    author: 'Christopher Alexander',
+    total: 44.53,
+    'order #': 1139984268120257,
+    'order placed': 'July 19, 2020',
+    delivered: 'July 20, 2020',
+    'return window closed': 'August 19, 2020',
+  },
+  {
+    title: 'GameCube Controller Adapter',
+    author: 'Knockoff',
+    total: 17.22,
+    'order #': 1142875557905940,
+    'order placed': 'July 4, 2020',
+    delivered: 'July 7, 2020',
+    'return window closed': 'August 5, 2020',
+  },
+  {
+    title: 'GameCube Controller - Super Smash Bros. Edition',
+    author: 'Nintendo',
+    total: 94.95,
+    'order #': 11328831772648248,
+    'order placed': 'July 3, 2020',
+    delivered: 'July 5, 2020',
+    'return window closed': 'August 4, 2020',
+  },
+  {
+    title: 'The Art of SQL',
+    author: 'Stephane Faroult',
+    total: 33.99,
+    'order #': 11328831772648248,
+    'order placed': 'July 3, 2020',
+    delivered: 'July 5, 2020',
+    'return window closed': 'August 4, 2020',
+  },
+];
+
+console.log('Value of orderHistory:', orderHistory);
