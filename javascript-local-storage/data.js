05dc74e43a1dc98e2f265e115343aa6fab1c720e
Tiffany Brown <tdbrown_2001@yahoo.com>
2023-11-13 17:00:49 +0000

    Made requested changes.

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -6,6 +6,8 @@ const previousTodosJSON = localStorage.getItem('javascript-local-storage');
 todos = JSON.parse(previousTodosJSON);
 
 window.addEventListener('beforeunload', (event) => {
-  const todosJSON = JSON.stringify(todos);
-  localStorage.setItem('javascript-local-storage', todosJSON);
+  if (previousTodosJSON !== null) {
+    const todosJSON = JSON.stringify(todos);
+    localStorage.setItem('javascript-local-storage', todosJSON);
+  }
 });
