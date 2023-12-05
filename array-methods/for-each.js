035941b679c3e18efad34bee169bf4ffabceb4e0
Tiffany Brown <tdbrown_2001@yahoo.com>
2023-12-05 02:50:16 +0000

    Made the requested changes.

--- array-methods/for-each.js
+++ array-methods/for-each.js
@@ -3,10 +3,14 @@ const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
 console.log('In order:');
 values.forEach((element) => console.log(element));
 
-const reversedArray = [];
+// const reversedArray = [];
 
-values.forEach((element) => {
-  reversedArray.unshift(element);
-});
+// values.forEach((element) => {
+//   values.unshift(element);
+// });
 console.log('Reverse order:');
-reversedArray.forEach((element) => console.log(element));
+// values.forEach((element) => console.log(element));
+
+values.forEach((element, index, array) => {
+  console.log(array[array.length - 1 - index]);
+});
