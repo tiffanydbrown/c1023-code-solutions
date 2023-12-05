2ad8691f48e6b6a224bbc8004319f6797c0af754
Tiffany Brown <tdbrown_2001@yahoo.com>
2023-12-05 02:53:52 +0000

    Deleted console logs and commented out code.

--- array-methods/for-each.js
+++ array-methods/for-each.js
@@ -3,13 +3,7 @@ const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
 console.log('In order:');
 values.forEach((element) => console.log(element));
 
-// const reversedArray = [];
-
-// values.forEach((element) => {
-//   values.unshift(element);
-// });
 console.log('Reverse order:');
-// values.forEach((element) => console.log(element));
 
 values.forEach((element, index, array) => {
   console.log(array[array.length - 1 - index]);
