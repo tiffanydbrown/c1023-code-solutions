0c50c1e2dbe305ffcf029fcc26d08ad46f0362aa
Tiffany Brown <tdbrown_2001@yahoo.com>
2023-12-13 04:18:56 +0000

    Completed es6-async-await exercise.

---
+++ es6-async-await/await.ts
@@ -0,0 +1,39 @@
+import { read } from './read.js';
+
+// Keep track of app load time so each log message can be timed.
+// Log messages should all be approximately 1 second apart.
+const startTime = Date.now();
+const elapsed = (): string =>
+  `${Math.round((Date.now() - startTime) / 1000)}s -`;
+
+async function readOnce(): Promise<void> {
+  const msg = await read('foo/bar.html');
+  console.log(elapsed(), 'readOnce:', msg);
+}
+
+async function readSeveral(): Promise<void> {
+  const msg = await read('foo1/bar.html');
+  console.log(elapsed(), 'readSeveral1:', msg);
+
+  const msg1 = await read('foo2/bar.html');
+  console.log(elapsed(), 'readSeveral2:', msg1);
+
+  const msg2 = await read('foo3/bar.html');
+  console.log(elapsed(), 'readSeveral3:', msg2);
+}
+
+async function readChained(): Promise<void> {
+  const msg = await read('foo-chain/bar.html');
+  console.log(elapsed(), 'readChained1:', msg);
+
+  const msg1 = await read(msg);
+  console.log(elapsed(), 'readChained2:', msg1);
+
+  const msg2 = await read(msg1);
+
+  console.log(elapsed(), 'readChained3:', msg2);
+}
+
+readOnce()
+  .then(() => readSeveral())
+  .then(() => readChained());
