ac023617a56328f2fc014bfe3f02f01cd1c20fe1
Tiffany Brown <tdbrown_2001@yahoo.com>
2023-12-20 19:12:13 +0000

    Worked on exercise #8.

--- practice-exercises/01-basic-challenges-1/08-palindrome/palindrome.ts
+++ practice-exercises/01-basic-challenges-1/08-palindrome/palindrome.ts
@@ -1,3 +1,6 @@
-export function isPalindrome(str: string): boolean {
-  return false;
-}
+// export function isPalindrome(str: string): boolean {
+//   const regex = /[^a-z0-9]/g;
+//   const alphaStr = str.replace(regex, '');
+//   const newStr = alphaStr.toLowerCase();
+//   return false;
+// }
