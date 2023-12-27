5c8b102ab294cde32a2125420cefbd66e20d6502
Tiffany Brown <tdbrown_2001@yahoo.com>
2023-12-27 20:35:51 +0000

    Completed #1-2 of section 2 in practices exercises.

--- practice-exercises/02-basic-challenges-2/02-array-intersection/array-intersection.ts
+++ practice-exercises/02-basic-challenges-2/02-array-intersection/array-intersection.ts
@@ -1,3 +1,10 @@
 export function arrayIntersection(arr1: unknown[], arr2: unknown[]): unknown[] {
-  return [];
+  const newArray = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i])) {
+      newArray.push(arr1[i]);
+    }
+  }
+  return newArray;
 }
