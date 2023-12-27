947c50d2a5de5c8a16f7c9e596c7f249072f7212
Tiffany Brown <tdbrown_2001@yahoo.com>
2023-12-27 18:34:30 +0000

    Completed node-notes-cli exercise.

--- node-notes-cli/app.ts
+++ node-notes-cli/app.ts
@@ -1,52 +1,84 @@
 import { readFile, writeFile } from 'node:fs/promises';
 
-// type Data = {
-//   nextId: number;
-//   notes: Record<string, string>;
-// };
+type Data = {
+  nextId: number;
+  notes: Record<string, string>;
+};
 
-const command = process.argv[2];
+const [, , command, arg1, arg2] = process.argv;
 
-switch (command) {
-  case 'read':
-    try {
-      const contents = await readFile('data.json', 'utf-8');
-      const contentObject = JSON.parse(contents);
-      console.log(contentObject);
-      for (const property in contentObject.notes) {
-        console.log(`${property}: ${contentObject.notes[property]}`);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-    break;
-  case 'create':
-    try {
-      const contents = await readFile('data.json', 'utf-8');
-      const contentObject = JSON.parse(contents);
-      contentObject.notes[contentObject.nextId] = process.argv[3];
-      const contentString = JSON.stringify(contentObject, null, 2);
-      contentObject.nextId++;
-      await writeFile('data.json', contentString, 'utf-8');
-    } catch (err) {
-      console.log(err);
-    }
-    break;
-  case 'update':
-    // try {
-    //   const contents = await readFile('data.json', 'utf-8');
-    //   const contentObject = JSON.parse(contents);
-    //   contentObject.notes[contentObject.nextId] = process.argv[3];
-    //   const contentString = JSON.stringify(contentObject, null, 2);
-    //   await writeFile('data.json', contentString, 'utf-8');
-    // } catch (err) {
-    //   console.log(err);
-    // }
-    break;
-  case 'delete':
-    break;
-  default:
-    console.log('Error: Incorrect command!');
-    process.exit(1);
+try {
+  switch (command) {
+    case 'read':
+      await readNote();
+      break;
+    case 'create':
+      await createNote(arg1);
+      break;
+    case 'update':
+      await updateNote(arg1, arg2);
+      break;
+    case 'delete':
+      await deleteNote(arg1);
+      break;
+    default:
+      console.log('Error: Incorrect command!');
+      process.exit(1);
+  }
+} catch (err) {
+  console.error(err);
+  process.exit(1);
 }
-// console.log(result);
+
+async function read(): Promise<Data> {
+  const contents = await readFile('data.json', 'utf-8');
+  return JSON.parse(contents);
+}
+
+async function create(data: Data): Promise<void> {
+  await writeFile('data.json', JSON.stringify(data, null, 2), 'utf-8');
+}
+
+function checkNote(note: string): void {
+  if (!note) {
+    console.log('No note');
+    process.exit(1);
+  }
+}
+
+function checkId(id: string, data: Data): void {
+  if (!(id in data.notes)) {
+    console.log(`${id} does not exist`);
+    process.exit(1);
+  }
+}
+
+async function readNote(): Promise<void> {
+  const data = await read();
+  for (const id in data.notes) {
+    console.log(`${id} - ${data.notes[id]}`);
+  }
+}
+
+async function createNote(note: string): Promise<void> {
+  checkNote(note);
+  const data = await read();
+  data.notes[data.nextId] = note;
+  data.nextId++;
+  await create(data);
+}
+
+async function updateNote(id: string, note: string): Promise<void> {
+  checkNote(note);
+  const data = await read();
+  checkId(id, data);
+  data.notes[id] = note;
+  await create(data);
+}
+
+async function deleteNote(id: string): Promise<void> {
+  const data = await read();
+  checkId(id, data);
+  delete data.notes[id];
+  await create(data);
+}
