3ded24db466fed0bbb37f546cd86f4752f04374e
Tiffany Brown <tdbrown_2001@yahoo.com>
2023-12-28 16:07:19 +0000

    Completed exercises #4-5 of section 2.

--- practice-exercises/02-basic-challenges-2/04-find-missing-number/find-missing-number.ts
+++ practice-exercises/02-basic-challenges-2/04-find-missing-number/find-missing-number.ts
@@ -1,3 +1,10 @@
 export function findMissingNumber(arr: number[]): number | undefined {
-  return undefined;
+  if (!arr || arr.length === 0) {
+    return undefined;
+  }
+  const n = arr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = 0;
+  const missingNumber = arr.reduce((acc, num) => acc + num, actualSum);
+  return expectedSum - missingNumber;
 }
