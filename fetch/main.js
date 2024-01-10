bfcd4656d2655e00362003fafbb1c4bf26797c2b
Tiffany Brown <tdbrown_2001@yahoo.com>
2024-01-10 22:58:57 +0000

    Made requested changes.

--- fetch/main.js
+++ fetch/main.js
@@ -1,6 +1,11 @@
 async function logData() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await response.json();
-  console.log(data);
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) throw new Error(`fetch Error ${response.status}`);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.error('fetch failed!', err);
+  }
 }
 logData();
